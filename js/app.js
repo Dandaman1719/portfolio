9a5af7d380b912c8346cdff1ed51a1b9bbe08aa8
daniel-c <dandaman1719@hotmail.com>
2022-02-25 11:49:20 -0700

    updated the headshot for daniel

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
+const logo = document.getElementsByClassName('.logo');
 
 function toggleButton() {
     navList.classList.toggle('show')
-}
+};
 
-hamburgerButton.addEventListener('click', toggleButton)
+function toggleLogo() {
+    logo.classList.toggle('show')
+};
+
+hamburgerButton.addEventListener('click', toggleButton, toggleLogo)
+
